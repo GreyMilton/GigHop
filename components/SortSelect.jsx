a81e8193cbc99f69d5f6b93ba07aae445279ec93
Grey Milton's MBP <grey.milton@gmail.com>
2021-12-08 16:22:57 +0000

    create sort by button which changes name depending on which of the two sort by options has been chosen, with text displaying current choice above it

--- components/SortSelect.jsx
+++ components/SortSelect.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Text, View} from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View} from 'react-native';
 
 export default function SortSelect({ navigation }) {
+
+  const [sortButtonTitles, setSortButtonTitles] = useState(['start time', 'distance from location']);
+
+  const [sortButtonIsPressed, setSortButtonIsPressed] = useState(false);
+
+  const onPressOfSortButton = () => {
+    if (sortButtonIsPressed) {
+      setSortButtonIsPressed(false);
+    } else {
+      setSortButtonIsPressed(true);
+    }
+  }
+
   return (
     <View>
-      <Text>SortSelect</Text>
+      <Text>Currently sorted by {sortButtonIsPressed ? sortButtonTitles[0] : sortButtonTitles[1]}</Text>
+      <Button title={`Sort by ${sortButtonIsPressed ? sortButtonTitles[1] : sortButtonTitles[0]}`} onPress={onPressOfSortButton}/>
     </View>
   );
 }
