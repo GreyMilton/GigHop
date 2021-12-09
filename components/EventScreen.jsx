97de499e9f3c528c65eceef9b3e4a1dbfc932fe4
Dominic <dominicfrancisharris@gmail.com>
2021-12-09 11:58:40 +0000

    Sets up base component for flexbox implementation

--- components/EventScreen.jsx
+++ components/EventScreen.jsx
@@ -21,66 +21,94 @@ export default function EventScreen(props) {
   }, []);
 
   const dataArray = [data]
-
-
-  const Item = ({ title, entryPrice, description, picture}) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-      <View style={{
-        height: 5,
-        width: "100%",
-        backgroundColor: "rgba(0,0,0,0.5)",
-        margin: 10
-        }} 
-      />
-      <Text>Entry Price: £{entryPrice.$numberDouble}</Text>
-      <View style={{
-        height: 5,
-        width: "100%",
-        backgroundColor: "rgba(0,0,0,0.5)",
-        margin: 10
-        }} 
-      />
-      <Text>About Event: {description}</Text>
-      <View style={{
-        height: 5,
-        width: "100%",
-        backgroundColor: "rgba(0,0,0,0.5)",
-        margin: 10
-        }} 
-      />
-      <Image 
-        style={{    
-          flex: 1,
-          aspectRatio: 1.5, 
-          resizeMode: 'contain',
-          }}
-        source={{
-            uri: picture
-        }}
-      />
-    </View>
-  )
+  console.log(dataArray)
 
   if (isLoading) return <Text>LOADING</Text>
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>CHOSEN EVENT SCREEN</Text>
-      <FlatList
-        data={dataArray}
-        keyExtractor={(item) => item._id}
-        renderItem={({item}) => {
-          return <Item 
-            title={item.event_name}
-            entryPrice={item.entry_price}
-            description={item.description}
-            picture={item.picture}
+    <View>
+      {dataArray.map((item) => {
+        return (
+          <View key={item._id}>
+          <Text>{item.event_name}</Text>
+          <Text>Entry Price: £{item.entry_price}</Text>
+          <Text>description={item.description}</Text>
+          <Image 
+          style={{    
+            flex: 1,
+            aspectRatio: 1.5, 
+            resizeMode: 'contain',
+          }}
+          source={{
+            uri: item.picture
+          }}
           />
-        }}
-      />
-    </SafeAreaView>
-  );
-}
+          </View>
+        )
+        }
+      )}
+    </View>
+  )
+};
+
+
+//   const Item = ({ title, entryPrice, description, picture}) => (
+//     <View style={styles.item}>
+//       <Text style={styles.title}>{title}</Text>
+//       <View style={{
+//         height: 5,
+//         width: "100%",
+//         backgroundColor: "rgba(0,0,0,0.5)",
+//         margin: 10
+//         }} 
+//       />
+//       <Text>Entry Price: £{entryPrice.$numberDouble}</Text>
+//       <View style={{
+//         height: 5,
+//         width: "100%",
+//         backgroundColor: "rgba(0,0,0,0.5)",
+//         margin: 10
+//         }} 
+//       />
+//       <Text>About Event: {description}</Text>
+//       <View style={{
+//         height: 5,
+//         width: "100%",
+//         backgroundColor: "rgba(0,0,0,0.5)",
+//         margin: 10
+//         }} 
+//       />
+//       <Image 
+//         style={{    
+//           flex: 1,
+//           aspectRatio: 1.5, 
+//           resizeMode: 'contain',
+//           }}
+//         source={{
+//             uri: picture
+//         }}
+//       />
+//     </View>
+//   )
+
+//   if (isLoading) return <Text>LOADING</Text>
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text>CHOSEN EVENT SCREEN</Text>
+//       <FlatList
+//         data={dataArray}
+//         keyExtractor={(item) => item._id}
+//         renderItem={({item}) => {
+//           return <Item 
+//             title={item.event_name}
+//             entryPrice={item.entry_price}
+//             description={item.description}
+//             picture={item.picture}
+//           />
+//         }}
+//       />
+//     </SafeAreaView>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
