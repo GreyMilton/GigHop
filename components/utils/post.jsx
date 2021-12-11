c441d5e74e4b646c0dbdf09a6aa8d8d7515110a7
James Jordan <jamesjordan@Jamess-MacBook-Pro.local>
2021-12-11 12:14:42 +0000

    adds a post request to the form

--- components/utils/post.jsx
+++ components/utils/post.jsx
@@ -1,5 +1,16 @@
 import { gigHopAPI } from "./get";
 
-export const PostNewEventDetails = () => {
-  return gigHopAPI.post("/events");
+export const PostNewEventDetails = (data) => {
+  console.log(data)
+  const options = {headers: {
+    'Content-Type': 'application/json',
+}}
+  return gigHopAPI.post("/events", {
+    Object, options
+  })
+  .then((res)=> {
+    console.log(res)
+  }).catch((err) => {
+    console.log(err)
+  });
 };
