ef25b61cb991cd75224e6b4748ab525c0e2fd81a
Grey Milton's MBP <grey.milton@gmail.com>
2021-12-14 21:30:13 +0000

    style bottom tab of tab nav

--- style-documents/navigators-styling.js
+++ style-documents/navigators-styling.js
@@ -2,9 +2,23 @@ import { StyleSheet } from 'react-native';
 
 
 const tabNavigatorStyles = StyleSheet.create({
+  tabBar: {
+    height: 64,
+    paddingBottom: 2,
+  },
+  tabBarLabel: {
+    fontSize: 11,
+  },
+  tabBarIcon: {
+    // padding: 4,
+  },
+  tabBarItem: {
+    // backgroundColor: 'red',
+    padding: 6,
+  },
   userAvatar: {
-    width: 26,
-    height: 26,
+    width: 30,
+    height: 30,
     borderRadius: 14,
   }
 })
