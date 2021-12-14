e5b8fa7a3fdd7e9bce5406a354e2e1f484acfa64
James Jordan <jamesjordan@Jamess-MacBook-Pro.local>
2021-12-14 12:25:43 +0000

    fixws patch and post requests

--- components/NewUserScreen.jsx
+++ components/NewUserScreen.jsx
@@ -2,9 +2,9 @@ import React, { useContext, useEffect, useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 import {Checkbox} from 'react-native-paper'
 import { Form, FormItem } from "react-native-form-component";
-import { PostNewUser, PostNewArtist, PostNewVenue } from "../utils/api-requests";
+import { PostNewUser, PostNewArtist, PostNewVenue, patchUserIsArtist, patchUserIsVenue } from "../utils/api-requests";
 
-export default function NewUserScreen() {
+export default function NewUserScreen({navigation}) {
     const [username, setUsername] = useState('');
     const [artistPicture, setArtistPicture] = useState('');
     const [checkedArtist, setCheckedArtist] = useState(false);
@@ -12,8 +12,8 @@ export default function NewUserScreen() {
     const [artistDescription, setArtistDescription] = useState('');
     const [genre, setGenre] = useState(''); 
     const [venueName, setVenueName] = useState('');
-    const [latitude, setLatitude] = useState(50);
-    const [longitude, setLongitude] = useState(-4);
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
     const [venueDescription, setVenueDescription] = useState('');
     const [venuePicture, setVenuePicture] = useState('');
     const [venueAddress, setVenueAddress] = useState('');
@@ -38,8 +38,8 @@ export default function NewUserScreen() {
         venueForm = (
             <View>
                 <FormItem label="Venue Name" value={venueName} onChangeText={(venueName) => {setVenueName(venueName)}} />
-                <FormItem label="Latitude" keyboardType={"decimal-pad"} value={latitude} onChangeText={(latitude) => {setLatitude(latitude)}} />
-                <FormItem keyboardType={"decimal-pad"} label="Longitude" value={longitude} onChangeText={(longitude) => {setLongitude(longitude)}} />
+                <FormItem label="Latitude" keyboardType={"decimal-pad"} value={latitude.toString()} onChangeText={(latitude) => {setLatitude(latitude)}} />
+                <FormItem keyboardType={"decimal-pad"} label="Longitude" value={longitude.toString()} onChangeText={(longitude) => {setLongitude(longitude)}} />
                 <FormItem label="Venue Description" value={venueDescription} onChangeText={(venueDescription) => {setVenueDescription(venueDescription)}} />
                 <FormItem label="Venue picture" value={venuePicture} onChangeText={(venuePicture) => {setVenuePicture(venuePicture)}} />
                 <FormItem label="Venue address" value={venueAddress} onChangeText={(venueAddress) => {setVenueAddress(venueAddress)}} />
@@ -50,8 +50,8 @@ export default function NewUserScreen() {
         let newUserData = {
             _id: username,
             picture: profilePicture,
-            artist: checkedArtist,
-            venue: checkedVenue,
+            artist: '',
+            venue: '',
             events: []
         };
         let newArtistData = {
@@ -66,22 +66,33 @@ export default function NewUserScreen() {
             user_id: username,
             venue_name: venueName,
             coordinates:{
-                latitude:latitude,
-                longitude: longitude
+                latitude:{'$numberDecimal': latitude},
+                longitude: {'$numberDecimal': longitude}
             },
             description: venueDescription,
             picture: venuePicture,
             address: venueAddress,
             upcoming_events: []
         }
-        PostNewUser(newUserData);
+        await PostNewUser(newUserData);
         
         if(checkedArtist) {
-            PostNewArtist(newArtistData)
+            let artistId = await PostNewArtist(newArtistData);
+            let isArtist = {
+                is_artist: {artist: artistId}
+            }
+            patchUserIsArtist(isArtist, username)
         }
         if(checkedVenue) {
-            PostNewVenue(newVenueData)
+            let venueId = await PostNewVenue(newVenueData);
+            let isVenue = {
+                is_venue: {venue: venueId}
+            }
+            patchUserIsArtist(isVenue, username)
         }
+
+        navigation.navigate('Find gigs');
+
     }
 
     return (
