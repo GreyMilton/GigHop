1c15087f268693020683ab9efc66818f58e181e2
Stuart Jauncey <stuart.jauncey@shift.online>
2021-12-14 09:27:16 +0000

    removes unnecessary variables and changes set invalid user on change

--- components/LogInScreen.jsx
+++ components/LogInScreen.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Text, SafeAreaView, View, ScrollView } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import { Form, FormItem } from 'react-native-form-component';
 import { UserContext } from '../contexts/UserContext';
 import { getAllUsers, getSingleUser } from '../utils/api-requests';
 
-export default function LogInScreen({ navigation }) {
+export default function LogInScreen() {
 
   const [userName, setUserName] = useState("");
   const [validUsers, setValidUsers] = useState([]);
   const [showInvalidUser, setInvalidUser] = useState(false);
-  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { setCurrentUser } = useContext(UserContext);
   
   useEffect(() => {
     getAllUsers()
@@ -42,7 +42,10 @@ export default function LogInScreen({ navigation }) {
       <Text>Please enter your username to login</Text>
       <Text>{errorMessage}</Text>
       <Form onButtonPress={onSubmit}>
-        <FormItem label="Username" value={userName} onChangeText={ (userName) => setUserName(userName) }/>
+        <FormItem label="Username" value={userName} onChangeText={ (userName) => {
+            setUserName(userName)
+            setInvalidUser(false)
+          } }/>
       </Form>
     </ScrollView>
   );
