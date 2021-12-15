411314f718b034045a476fab70264b8055919eff
James Jordan <jamesjordan@Jamess-MacBook-Pro.local>
2021-12-15 12:52:57 +0000

    makes confirmation button work

--- components/ConfirmationScreen.jsx
+++ components/ConfirmationScreen.jsx
@@ -1,49 +1,89 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button, ScrollView } from 'react-native';
 import { UserContext } from '../contexts/UserContext';
-import { getArtistById, getEventById } from '../utils/api-requests';
+import { getArtistById, getEventById, patchConfirmArtist } from '../utils/api-requests';
 
 export default function ConfirmationScreen() {
 
   const { currentUser } = useContext(UserContext);
   const [ associatedEventIds, setAssociatedEventIds ] = useState([]);
   const [ associatedEvents, setAssociatedEvents ] = useState([]);
+  const [artistInfo, setArtistInfo] = useState([]);
+  const [reload, setReload] = useState(true)
+
+
 
   useEffect(() => {
+    setAssociatedEvents([]);
     if (currentUser.artist) {
-      getArtistById(currentUser.artist)
-        .then((artist) => {
-          const upcomingEvents = artist.upcoming_events.map(event => {
-            return event.event_id
-          })
-          setAssociatedEventIds(upcomingEvents);
+      return getArtistById(currentUser.artist)
+      .then((res) => {
+          const upcomingEvents = res.upcoming_events.map(event => {
+            return event.event_id;
         })
-    }
-  }, [])
-
-  useEffect(() => {
-    associatedEventIds.forEach(eventId => {
-      getEventById(eventId).then((event) => {
-        setAssociatedEvents((prevEvents) => {
-          return [...prevEvents, event[0]];
+        return upcomingEvents
+      })
+      .then((res) =>  {
+        let events = res.map(async (eventId) => {
+          return await getEventById(eventId)
+          }
+        )
+        return Promise.all(events).then((res)=> {
+          return res
         })
       })
-    })
-  }, [associatedEventIds]);
+        .then((res) => {
+          setAssociatedEvents(res.flat())
+      })
+      .catch((err)=> {
+        console.log(err)
+      })
+    }
+  }, [reload])
 
-  console.log(associatedEvents)
+  // console.log(artistInfo)
+
+  console.log('***************************');
+
+
+
+
+  
 
   let eventList = associatedEvents.map((event) => {
+    const handleClick = () => {
+    
+    console.log(event)
+    let artist = event.authorised.artist;
+    let venue = event.authorised.venue;
+    console.log(event.artist, event.venue)
+    let data = {authorised: {artist: !artist, venue: venue}}
+    patchConfirmArtist(data, event._id)
+    setReload(!reload)
+ }
+    let authorised;
+    if (event.authorised.artist) {
+      authorised = <Text>True</Text>
+    } else {
+      authorised = <Text>False</Text>
+    }
     return (
       <View key={event.event_name}>
+        <View key={event._id}>
         <Text>{event.event_name}</Text>
+        <Text>{event.venue_info[0].venue_name}</Text>
+        <Text>{event.time_start}</Text>
+        <Text>{event.time_end}</Text>
+        <Text>{authorised}</Text>
+        <Button title="Confirm" onPress={handleClick}></Button>
+      </View>
       </View>
     )
   })
 
   return (
-    <View>
+    <ScrollView>
       {eventList}
-    </View>
+    </ScrollView>
   );
 }
